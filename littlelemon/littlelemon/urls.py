dd0e7cbd5ca2f331593b2ec169c1cdc4477d380a
felixb177 <felixbaringinsamosir@gmail.com>
2025-05-09 11:47:15 +0700

    repairing the whole project

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from myapp import views
+from restaurant import views
 
 router = DefaultRouter()
 router.register(r'tables', views.BookingViewSet)

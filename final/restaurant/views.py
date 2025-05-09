139245e73e37046a85a0367e8a6f1466581d6631
felixb177 <felixbaringinsamosir@gmail.com>
2025-05-09 11:18:37 +0700

    fix

---
+++ final/restaurant/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from .models import Menu,Booking
+from .serializers import menuSerializer, bookingSerializer
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    permission_classes = [IsAuthenticated]
+    serializer_class = menuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = menuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = bookingSerializer
+
+def index(request):
+    return render(request, "index.html", {})

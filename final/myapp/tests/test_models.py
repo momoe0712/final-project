2f54917b9a929466ddbfbce2582edbbd5f8ec4f1
felixb177 <felixbaringinsamosir@gmail.com>
2025-05-09 11:20:42 +0700

    deleting some files

--- final/myapp/tests/test_models.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-from myapp.models import Menu
-
-class TestMenu(TestCase):
-    def test_get_item(self):
-        item = Menu.objects.create(Title="IceCream", Price=80, Inventory=100)
-        self.assertEqual(str(item), "IceCream : 80")
